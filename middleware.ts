a8cd51237ee2a3bf330dc04a0d0d717dc204ec96
sorrycc <sorrycc@gmail.com>
2026-01-06 00:38:28 +0800

    chore: update middleware matcher to exclude txt files

--- middleware.ts
+++ middleware.ts
@@ -27,6 +27,6 @@ export function middleware(request: NextRequest) {
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico|icon.svg|apple-icon.png|manifest|_pagefind|install.sh|config.json|.*\\.tar\\.gz).*)'
+    '/((?!api|_next/static|_next/image|favicon.ico|icon.svg|apple-icon.png|manifest|_pagefind|install.sh|config.json|.*\\.tar\\.gz|.*\\.txt).*)'
   ]
 }
